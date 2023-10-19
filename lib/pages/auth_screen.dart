165240a880aab1c5eeecd4c6b0a03717e96d6b5f
iagocarvalho07 <71970033+iagocarvalho07@users.noreply.github.com>
2023-10-19 19:34:58 -0300

    Finalizando tela de AuthTenticação

--- lib/pages/auth_screen.dart
+++ lib/pages/auth_screen.dart
@@ -1,17 +1,46 @@
+
 import 'package:chatapp/components/auth_form.dart';
+import 'package:chatapp/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  bool _isloading = false;
+
+  void _handleSubmit(AuthFormData formData) {
+    setState(() => _isloading = true);
+    print("AuthPAge");
+    print(formData.email);
+    setState(() => _isloading = false);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: SingleChildScrollView(
-          child: AuthForm(),
-        ),
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(
+                onSubmit: _handleSubmit,
+              ),
+            ),
+          ),
+          if (_isloading)
+            Container(
+              decoration: const BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.5)),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+        ],
       ),
     );
   }
