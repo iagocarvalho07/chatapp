d5504bc79f7b4f4f24aa9fb4062c0118e2d39e6d
iagocarvalho07 <71970033+iagocarvalho07@users.noreply.github.com>
2023-10-19 14:01:31 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chatapp/pages/auth_screen.dart';
+import 'package:chatapp/pages/loading_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
+
+
---
+++ lib/pages/auth_screen.dart
@@ -0,0 +1,18 @@
+import 'package:chatapp/components/auth_form.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: SingleChildScrollView(
+          child: AuthForm(),
+        ),
+      ),
+    );
+  }
+}
